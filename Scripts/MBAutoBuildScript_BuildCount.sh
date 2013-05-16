a007e1a030fffac97f61d81af7863769d31ee2fc
BB9z <bb9z@myopera.com>
2013-05-16 10:20:11 +0800

    修复大小写错误导致找不到 PlistBuddy 的错误

--- Scripts/MBAutoBuildScript_BuildCount.sh
+++ Scripts/MBAutoBuildScript_BuildCount.sh
@@ -13,13 +13,13 @@ if [ $enableAutoBuildCount = 1 ]; then
 	if [ -n "$(find "$SRCROOT" -not \( -name .git -prune \) -newer "$buildCountRecordFile" -not -path "*.xcodeproj/xcuserdata*" -not -path "*.xcodeproj/project.xcworkspace*" -not -name ".DS_Store" -not -type d)" ]; then
 		infoPlistPath="${PROJECT_DIR}/${INFOPLIST_FILE}"
 
-		if [ -n $(/usr/libexec/Plistbuddy -c "Print CFBundleVersion" "$infoPlistPath") ]; then
+		if [ -n $(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$infoPlistPath") ]; then
 			if [ $autoBuildCountUseDataFormat = 1 ]; then
 				buildnum=$(date $autoBuildCountDataFormat)
 			else
 				buildnum=$("$scriptPath/UserBuildCount" "$buildCountRecordFile" "$USER")
 			fi
-			/usr/libexec/Plistbuddy -c "Set CFBundleVersion $buildnum" "$infoPlistPath"
+			/usr/libexec/PlistBuddy -c "Set CFBundleVersion $buildnum" "$infoPlistPath"
 			echo "将版本设置为 $buildnum"
 		else
 			echo "错误：找不到 Info.plist 中的 CFBundleVersion"
