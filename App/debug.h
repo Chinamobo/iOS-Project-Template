813eddfa9accc18ff2b4cfaa2b5805b9a89ea6a0
BB9z <bb9z@myopera.com>
2013-05-21 17:13:10 +0800

    调整dout开关，移除debug.h中的相关选项，移动到target设置中。默认打开DOUT_ASSERT_AT_ERROR

--- App/debug.h
+++ App/debug.h
@@ -2,12 +2,6 @@
 #ifndef App_debug_h
 #define App_debug_h
 
-#define DOUT_ASSERT_AT_ERROR 0
-#define DOUT_TREAT_ERROR_AS_EXCEPTION 0
-
-#define DOUT_ASSERT_AT_WANRNING 0
-#define DOUT_TREAT_WANRNING_AS_EXCEPTION 0
-
 /// API
 // 每次启动强制自动更新，忽略更新间隔检查
 #define DebugAPIUpdateForceAutoUpdate 0
