a99c38904bdb14f989ea133e26ba2ce0ca188ec2
BB9z <bb9z@me.com>
2014-05-06 16:58:10 +0800

    杂项更新

--- App/Launching/AppDelegate.m
+++ App/Launching/AppDelegate.m
@@ -19,11 +19,17 @@
     if (DebugEnableUncaughtExceptionHandler) {
         InstallUncaughtExceptionHandler();
     }
-    
-    // TODO: 如有可能，需要把模块初始化置后
-    [DataStack sharedInstance];
-    [API sharedInstance];
-    
+
+    // 通用模块设置，按需取消注释以启用
+    // 如有可能，需要把模块初始化置后
+//    [API sharedInstance];
+
+    // Core Data
+//    [DataStack sharedInstance];
+
+    // 全局点击空白隐藏键盘
+//    [RFKeyboard setEnableAutoDisimssKeyboardWhenTouch:YES];
+
     [self generalAppearanceSetup];
     return YES;
 }
--- App/main.m
+++ App/main.m
@@ -15,14 +15,6 @@
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        NSString *tt = [[NSDateFormatter GMTFormatter] stringFromDate:[NSDate date]];
-        douto(tt);
-
-        NSString *fff = @"有现车\n颜色有黑、白、红\n可优惠2000元";
-        douto([NSJSONSerialization stringWithJSONObject:@{@"rr": fff} options:0 error:nil]);
-
-
-
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
