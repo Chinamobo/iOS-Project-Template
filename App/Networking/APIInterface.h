3b8fdf6302ebf374460876910fc9e7affa0f0170
BB9z <bb9z@me.com>
2014-05-06 11:12:27 +0800

    更新接口定义

--- App/Networking/APIInterface.h
+++ App/Networking/APIInterface.h
@@ -1,25 +1,28 @@
 /**
     APIInterface
 
-    接口定义
-
-    其他模块不应直接访问这里
-    需要对外的在 API.h 中用 extern 导出
+    接口名列表，具体定义在 .m 中
+    接口定义参见 APIDefine.plist
  */
 
 #import <Foundation/Foundation.h>
 
-#pragma mark - 接口定义
-NSString *const APIURLDeployBase        = @"http://example.com";
-
 // 资源文件基地址
-NSString *const APIURLAssetsBase        = @"http://example.com";
+extern NSString *const APIURLAssetsBase;
+
+#pragma mark - APIName
 
 // 用户接口
-NSString *const APIURLLogin         = @"login";
-NSString *const APIURLForgetPassword= @"forgetPassword";
-NSString *const APIURLUserInfo      = @"userInfo";
-NSString *const APIURLCheckUpdate       = @"version";
+extern NSString *const APIURLLogin;
+extern NSString *const APIURLResetPassword;
+extern NSString *const APIURLChangePassword;
+extern NSString *const APIURLUserInfo;
+
+// 更新
+extern NSString *const APIURLCheckUpdate;
+
 
 #pragma mark - User deafult key
 
+
+#pragma mark - Notification
---
+++ App/Networking/APIInterface.m
@@ -0,0 +1,18 @@
+
+#pragma mark - 接口定义
+
+// 资源文件基地址
+NSString *const APIURLAssetsBase        = @"http://example.com";
+
+// 用户接口
+NSString *const APIURLLogin				= @"User Login";
+NSString *const APIURLResetPassword		= @"User Reset Password";
+NSString *const APIURLChangePassword	= @"User Change Password";
+NSString *const APIURLUserInfo          = @"User Info";
+
+// 更新
+NSString *const APIURLCheckUpdate       = @"Version";
+
+#pragma mark - User deafult key
+
+#pragma mark - Notification
