375091d16e8ce8f65e57422d436f3524cabc19e1
BB9z <bb9z@me.com>
2014-06-08 15:31:11 +0800

    改进 MBTextField

--- App/General/Appearance/MBTextField.h
+++ App/General/Appearance/MBTextField.h
@@ -7,13 +7,21 @@
     Apache License, Version 2.0
     http://www.apache.org/licenses/LICENSE-2.0
  */
-#import <UIKit/UIKit.h>
+#import "RFUI.h"
 
 /**
  TextField 基类
 
  主要用于外观定制
  */
-@interface MBTextField : UITextField
+@interface MBTextField : UITextField <
+    RFInitializing
+>
 
+/**
+ 文字与边框的边距
+ 
+ 默认上下 7pt，左右 10pt
+ */
+@property (assign, nonatomic) UIEdgeInsets textEdgeInsets;
 @end
--- App/General/Appearance/MBTextField.m
+++ App/General/Appearance/MBTextField.m
@@ -2,11 +2,26 @@
 #import "MBTextField.h"
 
 @implementation MBTextField
+RFInitializingRootForUIView
+
+- (void)onInit {
+    self.textEdgeInsets = UIEdgeInsetsMake(7, 10, 7, 10);
+}
+
+- (void)afterInit {
+
+}
 
 - (CGRect)textRectForBounds:(CGRect)bounds {
-    // 文字与边距的距离
-    UIEdgeInsets textMargin = UIEdgeInsetsMake(7, 10, 7, 10);
-    return UIEdgeInsetsInsetRect(bounds, textMargin);
+    return UIEdgeInsetsInsetRect(bounds, self.textEdgeInsets);
+}
+
+- (CGRect)editingRectForBounds:(CGRect)bounds {
+    return [self textRectForBounds:bounds];
+}
+
+- (CGRect)placeholderRectForBounds:(CGRect)bounds {
+    return [self textRectForBounds:bounds];
 }
 
 @end
