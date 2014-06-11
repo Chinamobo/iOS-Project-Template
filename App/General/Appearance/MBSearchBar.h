5d4bad71c09c8e2b0295d4589ff5661f28e70c21
BB9z <bb9z@me.com>
2014-06-11 21:19:51 +0800

    + iOS 7 风格的 MBSearchBar

---
+++ App/General/Appearance/MBSearchBar.h
@@ -0,0 +1,22 @@
+/*!
+    MBSearchBar
+    v 0.1
+
+    Copyright © 2014 Chinamobo Co., Ltd.
+    https://github.com/Chinamobo/iOS-Project-Template
+
+    Apache License, Version 2.0
+    http://www.apache.org/licenses/LICENSE-2.0
+ */
+#import "RFUI.h"
+
+/**
+ iOS 7 外观的 UISearchBar
+ 
+ 偷懒未设置 ScopeBar 的样式，如果有需要按 MBSegmentedControl 设置一下就好了
+ */
+@interface MBSearchBar : UISearchBar <
+    RFInitializing
+>
+
+@end
