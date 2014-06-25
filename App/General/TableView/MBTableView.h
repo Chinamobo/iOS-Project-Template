3c14fd32ce68ade208b981e86a2a432d5ad3b29c
BB9z <bb9z@me.com>
2014-06-25 16:38:43 +0800

    MBTableView 0.2. 把分页、数据获取等逻辑真正集成进来

--- App/General/TableView/MBTableView.h
+++ App/General/TableView/MBTableView.h
@@ -1,6 +1,6 @@
 /*!
     MBTableView
-    v 0.1
+    v 0.2
 
     Copyright © 2014 Chinamobo Co., Ltd.
     https://github.com/Chinamobo/iOS-Project-Template
@@ -12,22 +12,54 @@
 #import "MBTableViewPullToFetchControl.h"
 #import "RFTableViewCellHeightDelegate.h"
 
-
 /**
- 封装常用 table view 特性：
- 
- - 使用 Auto Layout 自动计算 cell 高度
- 
+ Table view 基类
+
+ dataSorce 要留空，这个类只支持单 section 的情形。
+
+ 封装了分页的数据获取（包括下拉刷新、上拉加载更多，数据到底处理），Auto layout cell 自动高度等功能。
+ 其 delegate 要实现 MBTableViewDelegate 中的方法。
  */
 @interface MBTableView : UITableView <
-    RFInitializing
+    RFInitializing,
+    UITableViewDataSource
 >
-
 @property (strong, nonatomic) NSMutableArray *items;
 
-@property (assign, nonatomic) int page;
+@property (strong, nonatomic) RFTableViewCellHeightDelegate *cellHeightManager;
+@property (strong, nonatomic) MBTableViewPullToFetchControl *pullToFetchController;
+
+@property (assign, nonatomic) NSInteger page;
 @property (assign, nonatomic) NSUInteger pageSize;
 
-@property (strong, nonatomic) MBTableViewPullToFetchControl *fetchControl;
-@property (strong, nonatomic) RFTableViewCellHeightDelegate *cellHeightManager;
+/// 刷新数据，会重置 cell 高度缓存
+- (void)reload;
 @end
+
+/**
+ 可选协议，标明 cell 有 item 属性
+ */
+@protocol MBTableViewCellEntityExchanging <NSObject>
+@property (strong, nonatomic) id item;
+@end
+
+/**
+ 这个协议比较特殊，RFTableViewCellHeightDelegate 的两个方法变成必须的
+
+ 数据获取可以 delegate 完全实现（相应的数据添加、刷新、状态控制都要自行处理），或只提供必须的参数
+ */
+@protocol MBTableViewDelegate <RFTableViewCellHeightDelegate>
+@optional
+/// 如果 delegate 不实现该方法，下面两个方法就是必须的了
+- (void)fetchItemsWithPageFlag:(BOOL)nextPage;
+
+- (NSString *)fetchAPIName;
+- (NSMutableDictionary *)fetchParameters;
+@end
+
+/*! Change log
+
+ 0.2
+ - 把分页、数据获取等逻辑真正集成进来
+
+ */
--- App/General/TableView/MBTableView.m
+++ App/General/TableView/MBTableView.m
@@ -3,6 +3,7 @@
 #import "API.h"
 
 @interface MBTableView ()
+@property (weak, nonatomic) id<MBTableViewDelegate> delegate;
 @end
 
 @implementation MBTableView
@@ -11,33 +12,84 @@ RFInitializingRootForUIView
 - (void)onInit {
     self.pageSize = APIConfigFetchPageSize;
     self.items = [NSMutableArray array];
+    self.dataSource = self;
 }
 
 - (void)afterInit {
-    id orgDelegate = self.delegate;
-    self.delegate = self.cellHeightManager;
-    self.cellHeightManager.delegate = orgDelegate;
+    [self cellHeightManager];
 
-    self.fetchControl.tableView = self;
+    MBTableViewPullToFetchControl *control = self.pullToFetchController;
+
+    @weakify(self);
+    [control setHeaderProcessBlock:^{
+        @strongify(self);
+        [self fetchItemsWithPageFlag:NO];
+    }];
+
+    [control setFooterProcessBlock:^{
+        @strongify(self);
+        [self fetchItemsWithPageFlag:YES];
+    }];
 }
 
 - (RFTableViewCellHeightDelegate *)cellHeightManager {
     if (!_cellHeightManager) {
         _cellHeightManager = [[RFTableViewCellHeightDelegate alloc] init];
+        _cellHeightManager.delegate = (id)self.delegate;
+        self.delegate = (id)_cellHeightManager;
     }
     return _cellHeightManager;
 }
 
-- (MBTableViewPullToFetchControl *)fetchControl {
-    if (!_fetchControl) {
-        _fetchControl = [[MBTableViewPullToFetchControl alloc] init];
+- (MBTableViewPullToFetchControl *)pullToFetchController {
+    if (!_pullToFetchController) {
+        _pullToFetchController = [[MBTableViewPullToFetchControl alloc] init];
+        _pullToFetchController.tableView = self;
     }
-    return _fetchControl;
+    return _pullToFetchController;
 }
 
-- (void)reloadData {
-    [self.cellHeightManager invalidateCellHeightCache];
-    [super reloadData];
+- (void)fetchItemsWithPageFlag:(BOOL)nextPage {
+    if ([self.delegate respondsToSelector:@selector(fetchItemsWithPageFlag:)]) {
+        [self.delegate fetchItemsWithPageFlag:nextPage];
+        return;
+    }
+
+    self.page = nextPage? self.page + 1 : 1;
+    NSMutableDictionary *dic = self.delegate.fetchParameters;
+    [dic addEntriesFromDictionary:@{ @"page" : @(self.page), @"pagesize" : @(self.pageSize) }];
+
+    [API requestWithName:self.delegate.fetchAPIName parameters:dic viewController:self.viewController loadingMessage:nil modal:NO success:^(AFHTTPRequestOperation *operation, NSArray *responseObject) {
+        if (nextPage) {
+            [self.items addObjectsFromArray:responseObject];
+        }
+        else {
+            [self.items setArray:responseObject];
+            [self.cellHeightManager invalidateCellHeightCache];
+        }
+        self.pullToFetchController.footerReachEnd = !!(responseObject.count < self.pageSize);
+        [self reloadData];
+    } completion:^(AFHTTPRequestOperation *operation) {
+        [self.pullToFetchController markProcessFinshed];
+    }];
+}
+
+- (void)reload {
+    if (_cellHeightManager) {
+        [self.cellHeightManager invalidateCellHeightCache];
+    }
+    [self reloadData];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.items.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:[self.delegate tableView:tableView cellReuseIdentifierForRowAtIndexPath:indexPath]];
+    RFAssert(cell, nil);
+    [self.delegate tableView:tableView configureCell:cell forIndexPath:indexPath offscreenRendering:NO];
+    return cell;
 }
 
 @end
